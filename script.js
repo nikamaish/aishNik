43320dc8583104d52774e168908e14a8948e5152
aish <aishnikam31@gmail.com>
2024-09-21 03:47:45 +0530

    :sparkles: done with about me section

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ function toggleMenu() {
     document.getElementById('navbar-items').classList.toggle('hidden', !isOpen);
     document.getElementById('menu-icon').setAttribute('d', isOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16m-7 6h7');
 }
+
+new Typed('#typed', {
+    strings: ["Gamer", "Developer", "Hustler"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: '|', // Optional: change the cursor character
+});
+
